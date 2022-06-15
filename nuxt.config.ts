a543c10175d3aac32740ad664ce9dd25000da9cf
Arnut Klamhui <kla.arnut@gmail.com>
2022-06-15 03:02:20 +0000

    update config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,13 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
+    vite: {
+        server: {
+            port: 443,
+            https: true,
+            hmr: {
+                protocol: 'wss'
+            }
+        }
+    }
 })
