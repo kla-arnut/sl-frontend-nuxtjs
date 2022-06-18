4524ac51804dbffc08b6db40b964f8541b0f3575
Arnut Klamhui <arnut@netwway.co.th>
2022-06-18 15:39:51 +0000

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,13 +4,9 @@ import { defineNuxtConfig } from 'nuxt'
 export default defineNuxtConfig({
     vite: {
         server: {
-            host: "0.0.0.0",
-            port: 3000,
-            strictPort: true,
             hmr: {
-                host: 'slproject.com',
-                port: 443,
-                protocol: 'wss'
+                protocol: 'ws',
+                host: 'localhost'
             },
         }
     }
